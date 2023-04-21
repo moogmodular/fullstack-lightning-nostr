edb9e6fbd6bca6054d48da08d10ec6d948071e15
Schlaus Kwab <schlauskwab@proton.me>
2023-04-21 13:51:06 +0200

    feat: overhaul

---
+++ packages/worker/src/index.ts
@@ -0,0 +1,170 @@
+import { initTRPC } from '@trpc/server'
+import { createHTTPServer } from '@trpc/server/adapters/standalone'
+import cron from 'node-cron'
+import { format } from 'date-fns'
+import dotenv from 'dotenv'
+import { RelayPool } from 'nostr-relaypool'
+import fetch from 'node-fetch'
+import { finishEvent, getPublicKey, Kind, nip04 } from 'nostr-tools'
+
+import { webcrypto } from 'crypto'
+import { PrismaClient } from 'db-prisma/index'
+
+global.crypto = webcrypto
+
+export type AppRouter = typeof appRouter
+
+const t = initTRPC.create()
+
+const publicProcedure = t.procedure
+const router = t.router
+
+dotenv.config({ path: '../../.env' })
+
+const relays = [
+    'wss://relay.damus.io',
+    'wss://offchain.pub',
+    'wss://nostr.wine',
+    'wss://nostr-pub.wellorder.net',
+    'wss://relay.nostr.info',
+    'wss://nostr.bitcoiner.social',
+    'wss://nostr.oxtr.dev',
+    'wss://nostr.fmt.wiz.biz',
+    'wss://relay.snort.social',
+]
+
+const appRouter = router({
+    greet: publicProcedure
+        .input((val: unknown) => {
+            if (typeof val === 'string') return val
+            throw new Error(`Invalid input: ${typeof val}`)
+        })
+        .query(({ input }) => ({ greeting: `hello, ${input}!` })),
+})
+
+const prisma = new PrismaClient()
+const startMentionSubscriptions = async () => {
+    const relayPool = new RelayPool(relays)
+
+    const test = await prisma.example.findMany()
+    const test2 = await prisma.helloEntity.findMany()
+
+    console.log('test', test)
+
+    const publicKey = getPublicKey(`${process.env.BOT_NOSTR_PRIVATE_KEY}`)
+    console.log('publicKey', publicKey)
+
+    const unsubscribe = relayPool.subscribe(
+        [
+            {
+                '#p': [publicKey, '', 'mention'],
+                kinds: [Kind.Text],
+                since: new Date().getTime() / 1000,
+            },
+        ],
+        relays,
+        (event, isAfterEose, relayURL) => {
+            // if ()
+            console.log('event', event)
+            const doEvent = async () => {
+                const privateKey = `${process.env.BOT_NOSTR_PRIVATE_KEY}`
+                const publicKey = getPublicKey(privateKey)
+
+                const price = (await fetch('https://api.kucoin.com/api/v1/market/stats?symbol=BTC-USDT').then((res) =>
+                    res.json(),
+                )) as { data: { buy: string } }
+
+                const unsignedEvent = {
+                    kind: 1,
+                    created_at: Math.floor(Date.now() / 1000),
+                    tags: [['e', event.id, '', 'root']],
+                    content: `DEV DEV Good morning ser,\nthe current price of BTC in USD is: \n ${price.data.buy}. ${event.content}`,
+                    pubkey: publicKey,
+                }
+
+                const finishedEvent = finishEvent(unsignedEvent, privateKey)
+
+                console.log('finishedEvent', finishedEvent)
+
+                relayPool.publish(finishedEvent, relays)
+
+                return finishedEvent
+            }
+            void doEvent()
+        },
+        undefined,
+        (events, relayURL) => {
+            console.log(events)
+        },
+    )
+}
+
+const startPrivateMessageSubscriptions = () => {
+    const relayPool = new RelayPool(relays)
+
+    const publicKey = getPublicKey(`${process.env.BOT_NOSTR_PRIVATE_KEY}`)
+
+    const unsubscribe = relayPool.subscribe(
+        [
+            {
+                '#p': [publicKey],
+                kinds: [Kind.EncryptedDirectMessage],
+                since: new Date().getTime() / 1000,
+            },
+        ],
+        relays,
+        (event, isAfterEose, relayURL) => {
+            // if ()
+            console.log('event', event)
+            const doEvent = async () => {
+                const privateKey = `${process.env.BOT_NOSTR_PRIVATE_KEY}`
+                const publicKey = getPublicKey(privateKey)
+
+                const price = (await fetch('https://api.kucoin.com/api/v1/market/stats?symbol=BTC-USDT').then((res) =>
+                    res.json(),
+                )) as { data: { buy: string } }
+
+                const message = `DEV DEV Good morning ser,\nthe current price of BTC in USD is: \n ${price.data.buy}. ${event.content}`
+                const ciphertext = await nip04.encrypt(privateKey, event.pubkey, message)
+
+                const unsignedEvent = {
+                    kind: Kind.EncryptedDirectMessage,
+                    created_at: Math.floor(Date.now() / 1000),
+                    tags: [['p', event.pubkey]],
+                    content: ciphertext,
+                    pubkey: publicKey,
+                }
+
+                const finishedEvent = finishEvent(unsignedEvent, privateKey)
+
+                console.log('finishedEvent', finishedEvent)
+
+                relayPool.publish(finishedEvent, relays)
+
+                return finishedEvent
+            }
+            void doEvent()
+        },
+        undefined,
+        (events, relayURL) => {
+            console.log(events)
+        },
+    )
+}
+const startCron = () => {
+    console.log('START CRON')
+    cron.schedule('30 * * * * *', () => {
+        console.log(`running a task every minute --- ${format(new Date(), 'hh:mm:ss')}`)
+    })
+}
+
+startCron()
+startMentionSubscriptions()
+startPrivateMessageSubscriptions()
+
+createHTTPServer({
+    router: appRouter,
+    createContext() {
+        return {}
+    },
+}).listen(2022)
