b5fa24d582afca37f8e9d5a1797b69eac11b1e7c
Schlaus Kwab <schlauskwab@proton.me>
2023-04-22 13:15:59 +0200

    feat: inst

---
+++ packages/db-prisma/prisma/migrations/20230421112450_hello/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "HelloEntity" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "HelloEntity_pkey" PRIMARY KEY ("id")
+);
